f5d526c28953e14979a3e3c3946d134d3bdae23f
Jacques Crocker <jcnetdev@gmail.com>
2008-07-05 17:56:54 -0700

    Removing unpacked gems

--- vendor/gems/jcnetdev-app_config-1.0/rails/init.rb
+++
@@ -1,4 +0,0 @@
-require 'app_config'
-
-::AppConfig = ApplicationConfig.load_files(RAILS_ROOT+"/config/app_config.yml",
-                                           RAILS_ROOT+"/config/environments/#{RAILS_ENV}.yml")
--- vendor/gems/jcnetdev-better_partials-1.0.200807042/rails/init.rb
+++
@@ -1,2 +0,0 @@
-require 'better_partials'
-ActionView::Base.send :include, BetterPartials::Helpers
